1c99277c3800bde6ec209b12a6cb311cadc3eab8
farashaidarp <49444214+farashaidarp@users.noreply.github.com>
2021-04-19 07:54:37 +0700

    Add files via upload

---
+++ md4/SpeakGoodBye.js
@@ -0,0 +1,16 @@
+(function(window){
+  
+  var byeSpeaker = {};
+  byeSpeaker.name = name;
+
+  
+  var speakWord = "Good Bye";
+
+  
+  byeSpeaker.speak = function (name){
+    console.log(speakWord + " " + name);
+  }
+  
+  window.byeSpeaker = byeSpeaker;
+}
+)(window);
